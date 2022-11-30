6d98e7607ee46801f3f88d80c05dcde082603c8c
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2022-11-30 03:44:48 +0000

    time est

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# logic-space
+# Logic Space
 All human reasoning should be represented as code to prevent reasoning fallacies
+
+Writing logical reasoning arguments as code helps to iron out the details of the logical train.
+This also exposes existing assumptions.
---
+++ time_estimation.py
@@ -0,0 +1,33 @@
+import datetime
+
+HOURS_A_DAY = 24
+HOURS_ASLEEP = 8
+WORKING_HOURS = 12
+WORKING_BLOCK = 6
+
+TIMING_CHAIN = ['1 min', '10 min', '1 day', '3 days', '7 days', '11 days', '17 days', '21 days']
+
+CARD_MATURE_TO_TIME_MAPPING = {
+    '1 min': 40,
+     '10 min': 30,
+     '1 day': 10,
+     '3 days': 25,
+     '7 days': 25,
+     '11 days': 25,
+     '17 days': 25,
+     '21 days': 25,
+}
+
+CARDS_TO_MATURITY = len(TIMING_CHAIN)
+
+
+HEAD_AND_NECK_ANATOMY = 3640
+
+TOTAL_SECONDS_TO_MATURITY = sum(CARD_MATURE_TO_TIME_MAPPING.values())
+
+def calclate_time_required(deck):
+    total_time = deck * TOTAL_SECONDS_TO_MATURITY
+    hours = total_time / 60 / 60
+    print('HOURS:', hours)
+    print('WORKING BLOCKS: ', hours / 6)
+    print('WORK DAYS: ', hours / 12)
